7ad0b8ace73f82d906f0c779e6bdfbb27a1b0aed
c2h30o2 <2143957645@qq.com>
2025-10-21 14:09:27 +0800

    backup

---
+++ net_cpp/net01.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+
+//协议
+// 1. TCP/IP 协议
+// 2. HTTP 协议
+// 3. FTP 协议
+// 4. SMTP 协议
+// 5. POP3 协议 
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;   
+}
